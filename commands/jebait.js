82eb48533b8ab5a7961c96bdff762f81097093a3
Tempeus <nekugm@gmail.com>
2021-02-27 12:30:03 -0500

    Update jebait.js

--- commands/jebait.js
+++ commands/jebait.js
@@ -1,4 +1,6 @@
-//This 
+//This plays sounds from the music folder. 
+//When the bot joins, it will play one soundbit that is categorised as Intro (it will have the prefix intro_)
+//Then it will play a random soundbit every 10 seconds.
 module.exports = {
     name: 'jebait',
     description: "insert tritin lines",
